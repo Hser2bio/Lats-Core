2b27892ff631a01c5e8b7bfd9f9aa8e6d6a2ae39
Liquid <Liquid369@gmail.com>
2022-10-31 09:35:30 -0500

    Update Ports

--- src/chainparams.h
+++ src/chainparams.h
@@ -9,9 +9,9 @@
 #ifndef BITCOIN_CHAINPARAMS_H
 #define BITCOIN_CHAINPARAMS_H
 
-#define __PORT_MAINNET__ 47773
-#define __PORT_TESTNET__ 19999
-#define __PORT_REGTEST__ 29999
+#define __PORT_MAINNET__ 45454
+#define __PORT_TESTNET__ 39999
+#define __PORT_REGTEST__ 49999
 
 #include "chainparamsbase.h"
 #include "checkpoints.h"
